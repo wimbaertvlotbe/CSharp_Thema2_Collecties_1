80c09f88c77c5ad4551f5f645b11efdcbb14e467
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2022-10-02 20:40:40 +0200

    Add project files.

---
+++ CSharp_Thema2_Collecties_1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace CSharp_Thema2_Collecties_1
+{
+    using System;
+    using System.Collections.Generic;
+
+
+        public class Program
+    {
+            static List<string> namen = new List<string>();
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Lees grondig door het onderstaande programma en schrijf de in de commentaar gevraagde zaken...");
+                Console.WriteLine("Indien nodig pas je de functiesignatuur aan...");
+            }
+
+            public static void AddName()
+            {
+
+                /*
+                 * Vraag in deze functie om een naam via Console.ReadLine()
+                 * Voeg deze naam toe aan de bovenstaande lijst. 
+                 */
+                throw new NotImplementedException();
+             }
+
+
+            public static void ShowList()
+            {
+
+                /* 
+                 * Toon via deze functie de lijst op het scherm
+                 */
+                throw new NotImplementedException();
+            }
+
+            public static void RemoveName()
+            {
+
+                /*
+                 * Vraag in deze functie om een naam via Console.ReadLine()
+                 * Verwijder deze naam uit de bovenstaande lijst.
+                 */
+                throw new NotImplementedException();
+            }
+
+
+            public static void SortNames()
+            {
+                /*
+                 * Sorteer via deze functie de lijst in alfabetische volgorde.
+                 */
+                throw new NotImplementedException();
+            }
+
+
+            public static void SortNamesReversed()
+            {
+                /*
+                 * Sorteer via deze functie de lijst in OMGEKEERD alfabetische volgorde.
+                 */
+                throw new NotImplementedException();
+            }
+
+            public static void Menu()
+            {
+   
+                /* 
+                 * Maak een menu met volgende opties:
+                 * 
+                 *"Voeg een naam toe"
+                 *"Verwijder een naam"
+                 *"Sorteer de lijst"
+                 *"Sorteer de lijst omgekeerd"
+                 *"Toon de lijst"
+                 *"Sluit de toepassing af"
+                 *
+                 *Programmeer elke functie zodat het gewenste gedrag wordt bereikt
+                 */
+            }
+        }
+}
